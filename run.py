a72df78d0ec15cdab2fcae49a824510797756040
cemkoc <cemkoc95@gmail.com>
2015-02-21 16:07:03 -0800

    caller integration

--- run.py
+++ run.py
@@ -1,14 +1,28 @@
-from flask import Flask
-import twilio.twiml
+from flask import Flask, request
+from twilio import twiml
+from twilio.rest import TwilioRestClient
+import os
  
 app = Flask(__name__)
+
+callers = {
+        "+14152983952": "Jem Koch"
+}
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming requests."""
+    from_number = request.values.get('From', None)
+    body = request.values.get('Body', None)
+
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
- 
+
+ 	if from_number in callers:
+                resp.message("Well hello there " + callers[from_number])
+                resp.message("You sent me: " + body)
+ 	else: 
+ 		resp.message("Hello, I don't know who you are.")
+ 		
     return str(resp)
  
 if __name__ == "__main__":
